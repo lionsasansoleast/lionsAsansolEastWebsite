23fc24da058fe3782c6b82f1836261991daf79ae
lionsasansoleast <lionsasansoleast@gmail.com>
2024-06-03 07:53:18 +0530

    initial commit

--- app/components/testimonials.tsx
+++ app/components/testimonials.tsx
@@ -41,7 +41,6 @@ const testimonials = [
     image:
       "https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80",
   },
-  
 ];
 
 const Testimonials = () => {
@@ -53,8 +52,8 @@ const Testimonials = () => {
         </h2>
 
         <div className="mt-8 [column-fill:_balance] sm:columns-2 sm:gap-6 lg:columns-3 lg:gap-8">
-          {testimonials.map((testimonial) => (
-            <div className="mb-8 sm:break-inside-avoid">
+          {testimonials.map((testimonial, index) => (
+            <div className="mb-8 sm:break-inside-avoid" key={index}>
               <blockquote className="rounded-lg bg-gray-50 p-6 shadow-sm sm:p-8">
                 <div className="flex items-center gap-4">
                   <img
